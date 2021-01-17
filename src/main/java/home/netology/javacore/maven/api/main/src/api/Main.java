213d6b1d7c7683e07ed22baa55d1ae96a592ceb4
Nikolay Dmukha <dmukha@mail.ru>
2021-01-17 15:08:23 +0300

    Netology. Maven task

---
+++ src/main/java/home/netology/javacore/maven/api/main/src/api/Main.java
@@ -0,0 +1,14 @@
+package api;
+
+import db.MyEntity;
+import service.MyService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyService myService = new MyService();
+        System.out.println(myService.getMyEntity());
+        System.out.println(myService.setMyEntity(new MyEntity("second")));
+        System.out.println(myService.getMyEntity());
+    }
+}
---
+++ src/main/java/home/netology/javacore/maven/service/main/src/service/MyService.java
@@ -0,0 +1,28 @@
+package service;
+
+import db.Db;
+import db.DbSetting;
+import db.MyEntity;
+
+import java.util.UUID;
+
+public class MyService {
+
+    private DbSetting dbSetting = new DbSetting("name", "password");
+    private String name = "myService";
+    private Db db = new Db(dbSetting);
+
+    public String getName() {
+        return name;
+    }
+
+    public MyEntity setMyEntity(MyEntity myEntity) {
+        myEntity.setId(UUID.randomUUID());
+        db.setMyEntity(myEntity);
+        return myEntity;
+    }
+
+    public MyEntity getMyEntity() {
+        return db.getMyEntity();
+    }
+}
