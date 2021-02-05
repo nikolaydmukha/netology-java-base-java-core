560029127169190d8ef551254e4ff70e53bd98ba
Nikolay Dmukha <dmukha@mail.ru>
2021-02-05 18:10:39 +0300

    Netology. Multithreading. Task 1

---
+++ src/main/java/home/netology/javacore/multithreading/task1/Main.java
@@ -0,0 +1,35 @@
+package home.netology.javacore.multithreading.task1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MyThread myThread1 = new MyThread("Поток 1");
+        MyThread myThread2 = new MyThread("Поток 2");
+        MyThread myThread3 = new MyThread("Поток 3");
+        MyThread myThread4 = new MyThread("Поток 4");
+
+        ThreadGroup tg = new ThreadGroup("Group 1");
+
+        Thread thread1 = new Thread(tg, myThread1);
+        Thread thread2 = new Thread(tg, myThread2);
+        Thread thread3 = new Thread(tg, myThread3);
+        Thread thread4 = new Thread(tg, myThread4);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+
+        try {
+            Thread.sleep(10000);
+            System.out.println("Завершаем потоки");
+            tg.interrupt();
+        }catch (InterruptedException ex){
+            System.out.println(ex.getMessage());
+        }
+
+    }
+}
---
+++ src/main/java/home/netology/javacore/multithreading/task1/MyThread.java
@@ -0,0 +1,24 @@
+package home.netology.javacore.multithreading.task1;
+
+public class MyThread extends Thread{
+
+    private String name;
+
+    public MyThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!isInterrupted()) {
+                Thread.sleep(2500);
+                System.out.println("Я " + getName() + ". Всем привет!");
+            }
+        } catch (InterruptedException err) {
+
+        } finally {
+            System.out.printf("%s завершен\n", getName());
+        }
+    }
+}
