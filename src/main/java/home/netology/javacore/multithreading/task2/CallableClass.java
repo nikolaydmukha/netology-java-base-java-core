5628eb820d6eb66ff223ef773411bc144923ef10
Nikolay Dmukha <dmukha@mail.ru>
2021-02-06 14:27:27 +0300

    Netology. Multithreading. Task 2

---
+++ src/main/java/home/netology/javacore/multithreading/task2/CallableClass.java
@@ -0,0 +1,18 @@
+package home.netology.javacore.multithreading.task2;
+
+import java.util.concurrent.Callable;
+
+class CallableClass implements Callable<String>
+{
+    @Override
+    public String call() throws Exception {
+        Thread.sleep(1000);
+        // возвращает имя потока, который выполняет callable таск
+        int i;
+        for(i = 0; i < 5; i ++){
+            Thread.sleep(1);
+            System.out.println(Thread.currentThread().getName() + " сообщение " + i);
+        }
+        return Thread.currentThread().getName() + " вывел " + i + " сообщений.";
+    }
+}
---
+++ src/main/java/home/netology/javacore/multithreading/task2/Main.java
@@ -0,0 +1,44 @@
+package home.netology.javacore.multithreading.task2;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        final ThreadFactory threadFactory = new ThreadFactoryBuilder()
+                .setNameFormat("Поток-%d")
+                .setDaemon(true)
+                .build();
+        //Получаем ExecutorService утилитного класса Executors с размером пула потоков равному 10
+        ExecutorService executor = Executors.newFixedThreadPool(4, threadFactory);
+        //создаем список с Future, которые ассоциированы с Callable
+        List<Future<String>> list = new ArrayList<>();
+        // создаем экземпляр CallableClass
+        Callable<String> callable = new CallableClass();
+        for(int i=0; i< 40; i++){
+            //сабмитим Callable таски, которые будут
+            //выполнены пулом потоков
+            Future<String> future = executor.submit(callable);
+            //добавляя Future в список,
+            //мы сможем получить результат выполнения
+            list.add(future);
+        }
+        for(Future<String> fut : list){
+            try {
+                // печатаем в консоль возвращенное значение Future
+                // будет задержка в 1 секунду, потому что Future.get()
+                // ждет пока таск закончит выполнение
+                System.out.println(new Date()+ "::" + fut.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        executor.shutdown();
+    }
+}
