7ce84801cff97b6c7426a5be7ab33c9eb5a04d72
Nikolay Dmukha <dmukha@mail.ru>
2021-02-14 14:41:28 +0300

    JavaRush. Task 16.*

---
+++ src/main/java/home/javarush/javaCore/task16/task1616/Solution.java
@@ -0,0 +1,42 @@
+package home.javarush.javaCore.task16.task1616;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Считаем секунды
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(in);
+        //create and run thread
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.start();
+        //read a string
+        reader.readLine();
+        stopwatch.interrupt();
+        //close streams
+        reader.close();
+        in.close();
+    }
+
+    public static class Stopwatch extends Thread {
+        private int seconds;
+
+        public void run() {
+            Thread current = Thread.currentThread();
+            while (!current.isInterrupted()) {
+                try {
+                    Thread.sleep(1000);
+                    seconds += 1;
+                } catch (InterruptedException e) {
+                    System.out.println(seconds);
+                    break;
+                }
+            }
+        }
+    }
+}
