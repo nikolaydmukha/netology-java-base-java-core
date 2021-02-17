a1f6cca2ce82884ba6f734684bcde492da2335b2
Nikolay Dmukha <dmukha@mail.ru>
2021-02-17 16:47:12 +0300

    JavaRush. Task 16.*

---
+++ src/main/java/home/netology/customutils/dateutils/Main.java
@@ -0,0 +1,16 @@
+package home.netology.customutils.dateutils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+        String srcDate =  "2024-09-19";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar c = Calendar.getInstance();
+        c.setTime(sdf.parse(srcDate));
+        c.add(Calendar.DATE, -20);
+        System.out.println(sdf.format(c.getTime()));
+    }
+}
