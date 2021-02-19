3a58d67ea0d038a206525842c81c6a937f1e9b73
Nikolay Dmukha <dmukha@mail.ru>
2021-02-19 15:01:00 +0300

    JavaRush. Task 16.*

--- src/main/java/home/netology/customutils/others/Main.java
+++ src/main/java/home/netology/customutils/others/Main.java
@@ -1,22 +1,49 @@
 package home.netology.customutils.others;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         //stream filter with conditions
-        List<Integer> arr  = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
-        List<Integer> statuses = new ArrayList<>();
-        statuses.add(3);
-        statuses.add(11);
-        statuses.add(6);
+        List<String> arr  = new ArrayList<>(Arrays.asList("1", "2", "3", "4"));
+        List<String> statuses = new ArrayList<>();
+        List<String> statuses2 = new ArrayList<>();
+        String value = null;
+        statuses.add("2");
+        statuses.add("11");
+        statuses.add("3");
+        statuses2.add("4");
 
-        List<Integer> cardDetails = arr.stream()
+        List<String> cardDetails = arr.stream()
                 .filter(c -> statuses.size() == 0 || statuses.contains(c))
+                .filter(c -> statuses2.size() == 0 || statuses2.contains(c))
+                .filter(c -> !getOptField(value).isPresent() || getOptField(value).get().equals(c))
                 .collect(Collectors.toList());
         System.out.println(cardDetails);
+        System.out.println(isParamExist("key"));
+    }
+
+    private static Optional<String> getOptField(String value) {
+        try {
+            return StringUtils.isBlank(value) ? Optional.empty() : Optional.of(value);
+        }catch(Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    private static  boolean isParamExist(String name) {
+        return !getApiFieldValue(name).isEmpty();
+    }
+
+    private static String getApiFieldValue(String name) {
+        Map<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        if(map.containsKey(name)) {
+            return map.get(name);
+        }
+        return null;
     }
 }
