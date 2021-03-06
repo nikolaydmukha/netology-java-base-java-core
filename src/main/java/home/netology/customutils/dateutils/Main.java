81cf021a9d590f9b675507cb97e123dc6b60f73b
Nikolay Dmukha <dmukha@mail.ru>
2021-03-06 09:44:20 +0300

    Rename src dir

--- src/main/java/home/netology/customutils/dateutils/Main.java
+++ src/main/java/home/netology/customutils/dateutils/Main.java
@@ -6,7 +6,7 @@ import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        String srcDate =  "2024-09-19";
+        String srcDate =  "2024-03-06";
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Calendar c = Calendar.getInstance();
         c.setTime(sdf.parse(srcDate));
