de1a95943af753dc6f315ea5f5dfbdec40564d76
Nikolay Dmukha <dmukha@mail.ru>
2021-03-19 14:23:17 +0300

    Rename src dir

---
+++ src/main/java/home/netology/customutils/others/Main2.java
@@ -0,0 +1,12 @@
+package home.netology.customutils.others;
+
+import java.time.LocalDate;
+
+public class Main2 {
+    public static void main(String[] args) {
+        LocalDate promisedDay = LocalDate.now().plusDays(5);
+        System.out.println(promisedDay);
+        LocalDate today = LocalDate.now();
+        System.out.println(promisedDay.isBefore(today.plusDays(5)));// || promisedDay.isEqual(today.plusDays(5)));
+    }
+}
