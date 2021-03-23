73a1923fc227db6424eed26f12a80b3988347f7c
Nikolay Dmukha <dmukha@mail.ru>
2021-03-23 15:53:46 +0300

    Some checks with dates

--- src/main/java/home/netology/customutils/others/Main2.java
+++ src/main/java/home/netology/customutils/others/Main2.java
@@ -1,11 +1,17 @@
 package home.netology.customutils.others;
 
+import java.time.Clock;
 import java.time.LocalDate;
 
 public class Main2 {
     public static void main(String[] args) {
         LocalDate promisedDay = LocalDate.now().plusDays(5);
-        LocalDate today = LocalDate.now();
+        LocalDate today = LocalDate.now(Clock.systemDefaultZone());
+
+        LocalDate payDate = LocalDate.parse("2021-03-23");
+        System.out.println(payDate);
+
+        System.out.println(String.valueOf(payDate.equals(today)));
         System.out.println(promisedDay.isBefore(today.plusDays(5)));// || promisedDay.isEqual(today.plusDays(5)));
     }
 }
