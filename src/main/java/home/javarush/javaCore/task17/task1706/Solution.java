eea55c27bbbab3b317c3ca5b0719b78db7d27375
Nikolay Dmukha <dmukha@mail.ru>
2021-03-25 20:31:52 +0300

    JavaRysh task1705, task1706

---
+++ src/main/java/home/javarush/javaCore/task17/task1706/Solution.java
@@ -0,0 +1,13 @@
+package home.javarush.javaCore.task17.task1706;
+
+/* 
+Синхронизированный президент
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        OurPresident expectedPresident = OurPresident.getOurPresident();
+        OurPresident ourPresident = OurPresident.getOurPresident();
+        System.out.println(expectedPresident == ourPresident);
+    }
+}
